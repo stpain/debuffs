caf3a8431be8f193cc2f3957abe31b6a45e5653e
Samuel Thomas Pain <sam.pain@live.co.uk>
2020-08-28 19:28:36 +0100

    removed time checks, addons operate locally

--- Debuffs.lua
+++ Debuffs.lua
@@ -21,9 +21,6 @@
 
 local _, Debuffs = ...
 
--- use this to convert from server times sent between players back to local times for cooldown widgets
-local home, server = GetTime(), GetServerTime()
-Debuffs.TimeDiff = (server-home)
 
 -- cooldown widget table
 Debuffs.CooldownFrames = {}
@@ -37,17 +34,23 @@ Debuffs.UNIT_SPELLCAST_SUCCEEDED_EVENT = {}
 -- table of spells to check when updatign cooldown widgets
 Debuffs.Spells = {
     -- priest
-    [589] = { Duration = 18, Name = '' }, -- shadow word: pain rank 1
+    [589] = { Duration = 18, Mod = 0 }, -- shadow word: pain rank 1
     -- warlock
-    [348] = { Duration = 15, Name = '' }, -- immolate rank 1
-    [172] = { Duration = 12, Name = '' }, -- corruption rank 1
-    [980] = { Duration = 24, Name = '' }, -- agony rank 1
-    [5782] = { Duration = 10, Name = '' }, -- fear rank 1
+    [348] = { Duration = 15, Mod = 0 }, -- immolate rank 1
+    [172] = { Duration = 12, Mod = 0 }, -- corruption rank 1
+    [6222] = { Duration = 12, Mod = 0 }, -- corruption rank 2
+    [6223] = { Duration = 12, Mod = 0 }, -- corruption rank 3
+    [7648] = { Duration = 12, Mod = 0 }, -- corruption rank 4
+    [11671] = { Duration = 12, Mod = 0 }, -- corruption rank 5
+    [980] = { Duration = 24, Mod = 0 }, -- agony rank 1
+    [5782] = { Duration = 10, Mod = 0 }, -- fear rank 1
+    [689] = { Duration = 5, Mod = 0 }, -- drain life rank 1
+    [1120] = { Duration = 15, Mod = 0 }, -- drain soul rank 1
     -- hunter
-    [5116] = { Duration = 4, Name = '' }, -- concussive shot
-    [1978] = { Duration = 15, Name = '' }, -- serpent string rank 1
-    [13549] = { Duration = 15, Name = '' }, -- serpent string rank 2
-    [13550] = { Duration = 15, Name = '' }, -- serpent string rank 3
+    [5116] = { Duration = 4, Mod = 0 }, -- concussive shot
+    [1978] = { Duration = 15, Mod = 0 }, -- serpent string rank 1
+    [13549] = { Duration = 15, Mod = 0 }, -- serpent string rank 2
+    [13550] = { Duration = 15, Mod = 0 }, -- serpent string rank 3
 }
 
 
@@ -93,27 +96,33 @@ end
 -- get data from message and update combat table, local vars required ??? maybe pass in directly from table
 function Debuffs:CHAT_MSG_ADDON(...)
     local prefix = select(1, ...)
-    if prefix == 'debuffs-cast' then
+    local sender = select(4, ...)
+    local player, realm = UnitFullName('player')
+    -- use this to test without filtering player
+    --if prefix == 'debuffs-cast' then
+    if prefix == 'debuffs-cast' and (sender ~= tostring(player..'-'..realm)) then
         local msg = select(2, ...)
         local tbl = {}
         for k in msg:gmatch("([^$]+)") do
             table.insert(tbl, k)
         end
-        local targetGUID, sourceGUID, spellID = tbl[1], tbl[2], tonumber(tbl[3])
-        -- convert server time back to local time with allowance for message delay
-        local start = tonumber(tbl[4] - self.TimeDiff) + (GetServerTime() - tbl[4])
-        -- print('ADDON MSG')
-        -- local spellName = select(1, GetSpellInfo(spellID))
-        -- print('spell', spellID, spellName)
-        -- print('start (server time)', tbl[4])
-        -- print('calc local start', start)
-        -- print('---------------------')
-        if not self.Combat[targetGUID] then
-            self.Combat[targetGUID] = {
-                [sourceGUID] = {}
-            }
+        if #tbl == 4 then
+            local targetGUID, sourceGUID, spellID = tbl[1], tbl[2], tonumber(tbl[3])
+            local start = GetTime() - 0.2
+            -- print('ADDON MSG')
+            -- local spellName = select(1, GetSpellInfo(spellID))
+            -- print('spell', spellID, spellName)
+            -- print('start (server time)', tbl[4])
+            -- print('server time', GetServerTime())
+            -- print('calc local start', start)
+            -- print('---------------------')
+            if not self.Combat[targetGUID] then
+                self.Combat[targetGUID] = {
+                    [sourceGUID] = {}
+                }
+            end
+            self.Combat[targetGUID][sourceGUID][spellID] = start
         end
-        self.Combat[targetGUID][sourceGUID][spellID] = start
     end
 end
 
@@ -141,27 +150,74 @@ end
 -- using this we know the spell landed without being blocked/resisted etc, this keeps start times accurate for cooldown widgets
 function Debuffs:COMBAT_LOG_EVENT_UNFILTERED(...)
     local cleu = {CombatLogGetCurrentEventInfo()}
+    local t = GetTime()
+    local s = GetServerTime()
+    local sourceGUID = cleu[4]
+    local targetGUID = cleu[8]
+
+    if cleu[2] == 'RANGE_AURA_APPLIED' or 'SPELL_AURA_APPLIED' then
+        print('AURA APPLIED CLEU - RANGE or SPELL')
+        print('local time:', t)
+        print('aura type:', cleu[15])
+        print('amount:', cleu[16])
+        print('--------------------'
+    end
+
+    if cleu[2] == 'RANGE_AURA_REFRESH' or 'SPELL_AURA_REFRESH' then
+        print('AURA REFRESHED CLEU - RANGE or SPELL')
+        print('local time:', t)
+        print('aura type:', cleu[15])
+        print('amount:', cleu[16])
+        print('--------------------'
+    end
+
+    if cleu[2] == 'SWING_AURA_APPLIED' then
+        print('AURA APPLIED CLEU - SWING')
+        print('local time:', t)
+        print('aura type:', cleu[12])
+        print('amount:', cleu[13])
+        print('--------------------'
+    end
+
+    if cleu[2] == 'SWING_AURA_REFRESH' then
+        print('AURA REFRESHED CLEU - SWING')
+        print('local time:', t)
+        print('aura type:', cleu[12])
+        print('amount:', cleu[13])
+        print('--------------------'
+    end
+
     if cleu[2] == 'SPELL_CAST_SUCCESS' then
-        local sourceGUID = cleu[4]
-        local targetGUID = cleu[8]
         -- local timestamp = cleu[1]
-        -- local timestamp_trimmed = tonumber(tostring(timestamp):sub(1, -5))
-        local t = GetTime()
+        -- --local timestamp_trimmed = tonumber(tostring(timestamp):sub(1, -5))
         -- print('CLEU')
+        -- print('server time', s)
         -- print('timestamp', timestamp)
         if self.UNIT_SPELLCAST_SUCCEEDED_EVENT and next(self.UNIT_SPELLCAST_SUCCEEDED_EVENT) then
-            local eventDelay = 1.0
-            if (self.UNIT_SPELLCAST_SUCCEEDED_EVENT.ServerTime >= (tonumber(tostring(cleu[1]):sub(1, -5)) - eventDelay)) and (self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SourceGUID == sourceGUID) then
-                --print('cleu event matches unit_spellcast event')
-                local inInstance, instanceType = IsInInstance()
-                if inInstance and (instanceType:lower() == 'party' or instanceType:lower() == 'raid') then
-                    C_ChatInfo.SendAddonMessage('debuffs-cast', tostring(targetGUID..'$'..sourceGUID..'$'..self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SpellID..'$'..self.UNIT_SPELLCAST_SUCCEEDED_EVENT.ServerTime), instanceType:upper())
-                else
-                    -- normal option is to just add directly 
-                    --self.Combat[targetGUID][sourceGUID][self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SpellID] = t -- use local time for our own data
+            --local eventDelay = 1.0
+            --if (self.UNIT_SPELLCAST_SUCCEEDED_EVENT.ServerTime >= (tonumber(tostring(cleu[1]):sub(1, -5)) - eventDelay)) and (self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SourceGUID == sourceGUID) then
+            if (self.UNIT_SPELLCAST_SUCCEEDED_EVENT.ServerTime == s) and (self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SourceGUID == sourceGUID) then
+                if not self.Combat[targetGUID] then
+                    self.Combat[targetGUID] = {
+                        [sourceGUID] = {}
+                    }
                 end
+                self.Combat[targetGUID][sourceGUID][self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SpellID] = (t - 0.2)
+                -- --print('cleu event matches unit_spellcast event')
+                -- local inInstance, instanceType = IsInInstance()
+                -- if inInstance and (instanceType:lower() == 'party' or instanceType:lower() == 'raid') then
+                --     C_ChatInfo.SendAddonMessage('debuffs-cast', tostring(targetGUID..'$'..sourceGUID..'$'..self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SpellID..'$'..self.UNIT_SPELLCAST_SUCCEEDED_EVENT.ServerTime), instanceType:upper())
+                -- else
+                --     -- normal option is to just add directly
+                --     if not self.Combat[targetGUID] then
+                --         self.Combat[targetGUID] = {
+                --             [sourceGUID] = {}
+                --         }
+                --     end
+                --     self.Combat[targetGUID][sourceGUID][self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SpellID] = t
+                -- end
                 -- use this to test sending data
-                C_ChatInfo.SendAddonMessage('debuffs-cast', tostring(targetGUID..'$'..sourceGUID..'$'..self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SpellID..'$'..self.UNIT_SPELLCAST_SUCCEEDED_EVENT.ServerTime), 'SAY')
+                -- C_ChatInfo.SendAddonMessage('debuffs-cast', tostring(targetGUID..'$'..sourceGUID..'$'..self.UNIT_SPELLCAST_SUCCEEDED_EVENT.SpellID..'$'..self.UNIT_SPELLCAST_SUCCEEDED_EVENT.ServerTime), 'SAY')
             end
         end
         --print('--------------------')
